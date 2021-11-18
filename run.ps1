1164be18518cc0bc88a97638bab205e30357c4af
Scott <scottdrichards@hotmail.com>
2021-11-18 17:33:17 -0500

    correct port numbers

--- run.ps1
+++ run.ps1
@@ -1,9 +1,9 @@
 ./build.ps1
 
 Start-Process powershell -ArgumentList "./bin/server.exe 1001 1002 1003"
-Start-Process powershell -ArgumentList "./bin/server.exe 1002 1001 1003"
-Start-Process powershell -ArgumentList "./bin/server.exe 1003 1001 1003"
-Start-Sleep -s 3
-Start-Process powershell -ArgumentList "./bin/client.exe 1001 2001 2002 2003"
-Start-Process powershell -ArgumentList "./bin/client.exe 1002 2001 2002 2003"
-Start-Process powershell -ArgumentList "./bin/client.exe 1003 2001 2002 2003"
+Start-Process powershell -ArgumentList "./bin/server.exe 1001 1002 1003"
+Start-Process powershell -ArgumentList "./bin/server.exe 1001 1002 1003"
+Start-Sleep -s 2
+Start-Process powershell -ArgumentList "./bin/client.exe 1001 2001 2002 2003 2004"
+Start-Process powershell -ArgumentList "./bin/client.exe 1002 2001 2002 2003 2004"
+Start-Process powershell -ArgumentList "./bin/client.exe 1003 2001 2002 2003 2004"
